20ed900af7ce82ebcf233c07e56e0b052680ac42
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-26 19:59:13 -0600

    Add command line support

--- word_search.py
+++ word_search.py
@@ -1,4 +1,5 @@
 from collections import namedtuple, defaultdict
+import sys
 
 PuzzleData = namedtuple('Puzzle_Data', ['words', 'field'])
 
@@ -29,10 +30,29 @@ class Puzzle:
         return f'Puzzle(words={self.words}, field={self.field})'
 
     def solve(self):
+        """Returns a dict of each search word, pointing to a list of found word instances.
+        Each word instance is a list of tuples representing coordinates in letter matrix."""
         solutions = defaultdict(list)
         for word in self.words:
             solutions[word].extend(self._search(word))
         return solutions
+
+    def pretty_solve(self):
+        """Wraps solve, and returns the solution as a string with the following format:
+        BONES: (0,6),(0,7),(0,8),(0,9),(0,10)
+        KHAN: (5,9),(5,8),(5,7),(5,6)
+        KIRK: (4,7),(3,7),(2,7),(1,7)
+        SCOTTY: (0,5),(1,5),(2,5),(3,5),(4,5),(5,5)
+        SPOCK: (2,1),(3,2),(4,3),(5,4),(6,5)
+        SULU: (3,3),(2,2),(1,1),(0,0)
+        SULU: (3,4),(3,3),(3,2),(3,1)
+        UHURA: (4,0),(3,1),(2,2),(1,3),(0,4)"""
+        solutions = self.solve()
+        for word in solutions:
+            for case in solutions[word]:
+                # converts a list of tuples into a string of comma separated str representations
+                coords = ','.join(map(lambda pos: repr(pos), case))
+                print(f'{word}: {coords}')
 
     def _search(self, word):
         """Searches self.field for word, searching horizontally left to right,
@@ -156,3 +176,14 @@ class Puzzle:
                     else:
                         solutions.append(solution)
         return solutions
+
+
+def main(argv):
+    with open(argv[0]) as file:
+        puzzle = Puzzle(*parse_puzzle(file))
+
+    puzzle.pretty_solve()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
